dad55fa3642b9f54019f9c5cd60cba3831e3ef32
Mansi Pattani <mansi.pattani@ishisystems.com>
2016-04-28 19:48:19 +0530

    Used SQLAlchemy and created files for different layers in examples

---
+++ SQLAlchemyExample/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response
+
+
+class MyResponse(Response):
+     default_mimetype = 'application/xml'
+
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    app.config.from_object(config_filename)
+    app.response_class = MyResponse
+
+    from app.users.models import db
+    db.init_app(app)
+
+    # Blueprints
+    from app.users.views import users
+    app.register_blueprint(users, url_prefix='/api/v1/users')
+
+    return app
